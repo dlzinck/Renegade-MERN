4fb0dd17def86725404fe53012aaec571ed68832
jpcreativeworks <jp.graphics.011@gmail.com>
2022-05-18 18:38:30 -0700

    skeleton files and a connected home component

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './app/components/Home/Home'
+
+
+function App() {
+  return (
+    <>
+    <div className="App">     
+       <h1>Renegade Attire Main</h1>
+    </div>
+    <Home />
+    </>
+  );
+}
+
+export default App;
