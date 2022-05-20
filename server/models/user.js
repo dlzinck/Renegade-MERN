4d3a17dd1c2d545a55c6e1ae86b1c38c08000512
Drew Zinck <dlzincktheii@gmail.com>
2022-05-20 12:02:38 -0700

    updated user model to use bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,34 +1,40 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-// Schema to create Student model
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      min_length: 1,
-      max_length: 128,
-    },
-    password: {
-      type: String,
-      required: true,
-      min_length: 1,
-      max_length: 128,
-    },
-    email: {
-      type: String,
-      required: true,
-      min_length: 1,
-      max_length: 50,
-    },
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 5,
   },
-  {
-    toJSON: {
-      getters: true,
-    },
-  }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+});
 
-const User = model("user", userSchema);
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
 
 module.exports = User;
