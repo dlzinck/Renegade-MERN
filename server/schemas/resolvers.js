3f58e213097d5c0de93e4a226d7d0947bcffa397
Drew Zinck <dlzincktheii@gmail.com>
2022-05-20 13:00:50 -0700

    Added login function and exported resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,4 +14,20 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
+
+module.exports = resolvers;
