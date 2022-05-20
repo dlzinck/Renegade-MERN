e8654678ec299932474f9200f36f051bb751719c
Drew Zinck <dlzincktheii@gmail.com>
2022-05-20 11:53:37 -0700

    added findone for resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,11 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate("user");
+    },
+  },
+};
