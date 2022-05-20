f41bd3cd2292ddd32acaf9b4c1c85f8a4c63b02d
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-05-19 17:31:07 -0700

    made some changes to api routes

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 128,
+    },
+    password: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 128,
+    },
+    email: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 50,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Student = model('user', studentSchema);
+
+module.exports = Student; 
