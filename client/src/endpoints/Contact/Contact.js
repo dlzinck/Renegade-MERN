413e4cb90ecea9f81b06b94bf8d5cc7bb8bd9c5a
jpcreativeworks <jp.graphics.011@gmail.com>
2022-05-22 16:53:48 -0700

    made contact form for client and testing file on server side called emailserver updated packages do npm i after pull

--- client/src/endpoints/Contact/Contact.js
+++ client/src/endpoints/Contact/Contact.js
@@ -1,17 +1,78 @@
-import React from 'react';
+import React, { useState } from "react";
 function Contact() {
-    return (
-        <div>
-            
-            <h1>Contact Us</h1>
-            <form> 
-                <input type="text"></input>
-                <input type="text"></input>
-                <button>SUBMIT</button>
-            </form>
-            
-        </div>
-    );
+  const [submittedContact, setSubmittedContact] = useState(false);
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleContactSubmit = (event) => {
+    event.preventDefault();
+    setSubmittedContact(true);
+    const data = { name: name, message: message, email: email };
+    const url = "http://localhost:8000/"; //change when gets depoloyed to production
+    const response = fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    response.then((result) => {
+      return result.json();
+    });
+  };
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <form>
+        <fieldset>
+          <label htmlFor="name">Name:</label>
+
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Your email address"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="message">Message:</label>
+          <input
+            type="text"
+            name="message"
+            placeholder="Your Message"
+            value={message}
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+          />
+        </fieldset>
+        <button onClick={handleContactSubmit}>SUBMIT</button>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
