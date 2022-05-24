230e716bd8aa8128263cdc5f1207d12e2ed36c09
jpcreativeworks <jp.graphics.011@gmail.com>
2022-05-23 17:22:54 -0700

    about section is linked on homepage of navbar

--- client/src/endpoints/Home/Home.js
+++ client/src/endpoints/Home/Home.js
@@ -1,15 +1,25 @@
 // importing react 
 import React from 'react';
+import Card from '../../components/ProductCard/ProductCard';
+import About from '../../components/About/About';
 
 //importing components 
-
+const items = [{
+    id: "1",
+    title: "Poha",
+    category: "Breakfast",
+    price: "$1",
+    img: "https://c.ndtvimg.com/2021-08/loudr2go_aloo-poha_625x300_05_August_21.jpg?im=FeatureCrop,algorithm=dnn,width=620,height=350",
+    desc: " Poha. Light, filling and easy to make, poha is one famous breakfast that is eaten almost everywhere in the country. And the best part is- it can be made in a number of ways. Kanda poha, soya poha, Indori poha, Nagpur Tari Poha are a few examples",
+}];
 function Home() {
     return (
         <div>
             
-            <h1>Renegade</h1>
-            <p>Welcome! lets shop</p>
-
+            <img src='/images/RenegadeAttire_Landing.png' alt='landing page hero'></img>
+            <Card item={items}/>
+                        
+            <About />
         
         </div>
     );
